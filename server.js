de149e0419f53883c09b4d411ff5d1b2204bfe94
leni lunderman <lenilunderman@gmail.com>
2020-10-07 09:22:45 -0400

    Redirecting method

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const fs = require('fs')
+const { getHeapCodeStatistics } = require('v8')
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
@@ -12,12 +13,20 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
       path += 'index.html'
+      res.statusCode = 200
       break
     case '/about':
       path += 'about.html'
+      res.statusCode = 200
+      break
+    case '/about-me':
+      res.statusCode = 301
+      res.setHeader('Location', '/about')
+      res.end()
       break
     default:
       path += '404.html'
+      res.statusCode = 404
   }
 
   // sending page to the website.
@@ -36,3 +45,10 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
   console.log('listing for request on port 3000')
 }) // in order to listen the server...
+
+// status code
+// 100 range - informational responses
+// 200 range - success codes
+// 300 range - codes for redirects
+// 400 range - user or client errros codes
+// 500 range - server error codes
