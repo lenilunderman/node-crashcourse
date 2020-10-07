da33939d82db622fcef7c671d5e595a4a3bb57e2
leni lunderman <lenilunderman@gmail.com>
2020-10-07 12:31:11 -0400

    usingig the EJS package and engine for express -L6

--- app.js
+++ app.js
@@ -3,26 +3,33 @@ const express = require('express')
 
 // create an express app
 const app = express()
+app.set('view engine', 'ejs') // using ejs for views and templates
 // listen to the port of the server
 app.listen(3000)
 
 // routers
 app.get('/', (req, res) => {
   //res.send('home')
-  res.sendFile('./views/index.html', { root: __dirname })
+  res.render('index')
 })
 
 app.get('/about', (req, res) => {
   //res.send('./views/about.html')
-  res.sendFile('./views/about.html', { root: __dirname })
+  res.render('about')
 })
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+  //res.send('./views/about.html')
+  res.render('create')
 })
+
 
 // 404 pages...
 app.use((req, res) => {
-  res.sendFile('./views/404.html', { root: __dirname })
+  res.status(404).render('404')
 })
+
+// redirects
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/about')
+// })
