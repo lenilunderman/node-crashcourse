4dc45eac043ed4ab9cb61970c30dafd956f1cd36
leni lunderman <lenilunderman@gmail.com>
2020-10-07 12:49:43 -0400

    ejs templates and server side rendering

--- app.js
+++ app.js
@@ -9,24 +9,30 @@ app.listen(3000)
 
 // routers
 app.get('/', (req, res) => {
-  //res.send('home')
-  res.render('index')
+  // passing static objects into the ejs pages
+  const blogs = [
+    { title: 'Blog1', snippet: 'lorem 1 blog 1 yeah' },
+    { title: 'Blog2', snippet: 'lorem 2 blog 2 yeah' },
+    { title: 'Blog3', snippet: 'lorem 3 blog 3 yeah' },
+  ]
+
+  res.render('index', { title: 'Homepage', blogs })
 })
 
 app.get('/about', (req, res) => {
   //res.send('./views/about.html')
-  res.render('about')
+  res.render('about', { title: 'About' })
 })
 
 app.get('/blogs/create', (req, res) => {
   //res.send('./views/about.html')
-  res.render('create')
+  res.render('create', { title: 'Create Blog' })
 })
 
 
 // 404 pages...
 app.use((req, res) => {
-  res.status(404).render('404')
+  res.status(404).render('404', { title: '404' })
 })
 
 // redirects
