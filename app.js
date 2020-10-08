0bbb6c719a4fbaae224c08fadb5e6c45ab14a782
leni lunderman <lenilunderman@gmail.com>
2020-10-08 08:40:38 -0400

    create posts in mongoDB

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express')
 const morgan = require('morgan') //logger middleware
 const mongoose = require('mongoose')
 
+// access the database, model folder to get the model
+const Blog = require('./models/blog')
+
 // create an express app
 const app = express()
 
-//?? connection to mongoDB
+//?? connection to mongoDB - user - pass - database name
 const database = 'mongodb+srv://lenilunderman:M@forty42two@nodeleni.az2vk.mongodb.net/leni-database?retryWrites=true&w=majority'
 // using mongoose to connect
 mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,6 +20,7 @@ mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
 app.set('view engine', 'ejs') //!! using ejs for views and templates
 
 // middlewares
+app.use(express.urlencoded({ extended: true })) // middleware to encode to json format *IMPORTANT*
 app.use(express.static('public')) //!! tells what folder is public inside the project, so the css can go in here
 app.use(morgan('tiny'))
 // app.use((req, res, next) => {  // add the next, so the express knows what to do next..
@@ -31,13 +35,14 @@ app.use(morgan('tiny'))
 // routers
 app.get('/', (req, res) => {
   // passing static objects into the ejs pages
-  const blogs = [
-    { title: 'Blog1', snippet: 'lorem 1 blog 1 yeah' },
-    { title: 'Blog2', snippet: 'lorem 2 blog 2 yeah' },
-    { title: 'Blog3', snippet: 'lorem 3 blog 3 yeah' },
-  ]
+  // const blogs = [
+  //   { title: 'Blog1', snippet: 'lorem 1 blog 1 yeah' },
+  //   { title: 'Blog2', snippet: 'lorem 2 blog 2 yeah' },
+  //   { title: 'Blog3', snippet: 'lorem 3 blog 3 yeah' },
+  // ]
 
-  res.render('index', { title: 'Homepage', blogs })
+  // res.render('index', { title: 'Homepage', blogs })
+  res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
@@ -45,16 +50,84 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' })
 })
 
+// blog routes
+app.get('/blogs', (req, res) => {
+  // Access the schema to be used and find all posts
+  Blog.find().sort({ createdAt: -1 }) // since it takes time, return a promise
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+app.post('/blogs', (req, res) => {
+  //console.log(req.body)
+  const blog = new Blog(req.body)
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+
 app.get('/blogs/create', (req, res) => {
   //res.send('./views/about.html')
   res.render('create', { title: 'Create Blog' })
 })
 
-
 // 404 pages...
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' })
 })
+
+
+/* sandbox for mongoose and mongodb database */
+// app.get('/add-blog', (req, res) => {
+//   // create a new instance and save to the blog collection
+//   const blog = new Blog({
+//     title: 'gene',
+//     snippet: 'the blog about bears',
+//     body: 'more information soon'
+//   })
+
+//   blog.save()  // since it takes some time, it retuns a promise.
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+
+// })
+
+// // get all blogs
+// app.get('/all-blogs', (req, res) => {
+//   //name of the collection . find() to list all.
+//   Blog.find()
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
+
+// // get single post
+// app.get('/single-blog', (req, res) => {
+//   //name of the collection . find() to list all.
+//   Blog.findById('5f7e91df94695b5f69b5ebbc')
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
 
 // redirects
 // app.get('/about-us', (req, res) => {
