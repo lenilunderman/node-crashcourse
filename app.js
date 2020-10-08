324e4fa7f8caa24cfd4ef2ad297ac6d6b41624cd
leni lunderman <lenilunderman@gmail.com>
2020-10-08 09:43:32 -0400

    Final project

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 const express = require('express')
 const morgan = require('morgan') //logger middleware
 const mongoose = require('mongoose')
+const blogRoutes = require('./routes/blogRoutes')
 
-// access the database, model folder to get the model
-const Blog = require('./models/blog')
 const { render } = require('ejs')
 
 // create an express app
@@ -52,56 +51,7 @@ app.get('/about', (req, res) => {
 })
 
 // blog routes
-app.get('/blogs', (req, res) => {
-  // Access the schema to be used and find all posts
-  Blog.find().sort({ createdAt: -1 }) // since it takes time, return a promise
-    .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
-
-app.post('/blogs', (req, res) => {
-  //console.log(req.body)
-  const blog = new Blog(req.body)
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs')
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
-
-app.get('/blogs/:id', (req, res) => {
-  const id = req.params.id
-  Blog.findById(id)
-    .then(result => {
-      res.render('details', { blog: result, title: 'Blog Details' })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-      res.json({ redirect: '/blogs' })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
-
-
-app.get('/blogs/create', (req, res) => {
-  //res.send('./views/about.html')
-  res.render('create', { title: 'Create Blog' })
-})
+app.use(blogRoutes)
 
 // 404 pages...
 app.use((req, res) => {
---
+++ routes/blogRoutes.js
@@ -0,0 +1,58 @@
+const express = require('express')
+// access the database, model folder to get the model
+const Blog = require('../models/blog')
+
+const router = express.Router()
+
+// blog routes
+router.get('/blogs', (req, res) => {
+  // Access the schema to be used and find all posts
+  Blog.find().sort({ createdAt: -1 }) // since it takes time, return a promise
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+router.post('/blogs', (req, res) => {
+  //console.log(req.body)
+  const blog = new Blog(req.body)
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+router.get('/blogs/create', (req, res) => {
+  //res.send('./views/about.html')
+  res.render('create', { title: 'Create Blog' })
+})
+
+router.get('/blogs/:id', (req, res) => {
+  const id = req.params.id
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+router.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+module.exports = router
